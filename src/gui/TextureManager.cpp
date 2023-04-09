42fa76e549b6ba2a6ed82eeb64f067076a1f14c2
Youssef El Shabrawii <youssef.elshabrawii@gmail.com>
2023-04-10 01:13:23 +0200

    Creating Strucure::TextureManager - .h, .cpp

---
+++ src/gui/TextureManager.cpp
@@ -0,0 +1,13 @@
+#include "include/TextureManager.h"
+
+SDL_Texture* TextureManager::LoadTexture(const char* fileName){
+    SDL_Surface* tmpSurface = IMG_Load(GET(fileName));
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(Game::renderer, tmpSurface);
+    SDL_FreeSurface(tmpSurface);
+
+    return tex;
+}
+
+void TextureManager::Draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest){
+    SDL_RenderCopy(Game::renderer, tex, &src, &dest);
+}
