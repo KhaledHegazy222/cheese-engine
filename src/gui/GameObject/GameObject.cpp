c416da8ea134adaa936f091dc341c7bb7c3dad5c
Youssef El Shabrawii <youssef.elshabrawii@gmail.com>
2023-04-11 01:26:44 +0200

    Adding Structure::GameObject - .cpp, .h

---
+++ src/gui/GameObject/GameObject.cpp
@@ -0,0 +1,20 @@
+#include "GameObject.h"
+#include "../TextureManager/TextureManager.h"
+
+GameObject::GameObject(const char* texturesheet, SDL_Rect _srcRect, SDL_Rect _desRect){
+    objTexture = TextureManager::LoadTexture(texturesheet);
+    srcRect = _srcRect;
+    desRect = _desRect;
+}
+GameObject::~GameObject(){
+
+}
+
+void GameObject::Update(int x_des, int y_des){
+    desRect.x = x_des;
+    desRect.y = y_des;
+}
+
+void GameObject::Render(){
+    TextureManager::Draw(objTexture, srcRect, desRect);
+}
---
+++ src/gui/GameObject/GameObject.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <gui/Game.h>
+
+class GameObject {
+    
+    public:
+        GameObject(const char* texturesheet, SDL_Rect srcRect, SDL_Rect desRect);
+        ~GameObject();
+        void Update(int x, int y);
+        void Render();
+
+    private:
+        SDL_Texture* objTexture;
+        SDL_Rect srcRect, desRect;
+        
+
+};
