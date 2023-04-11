6e448357d6bc7e6f64fc7ee3d37ebdc3a50ea0fa
Khaled Hegazy <52636794+KhaledHegazy222@users.noreply.github.com>
2023-04-12 00:18:10 +0200

    Add screens navigation (#13)
    * ScreenHandler class which includes these methods:
       - initialize screen
       - update screen
       - switch screen
       - render screen
       - cleanup screen
       - handle events

--- src/gui/Screens/HomeScreen/HomeScreen.cpp
+++ src/gui/Screens/HomeScreen/HomeScreen.cpp
@@ -35,8 +35,10 @@ void Button::render() {
     SDL_RenderFillRect(Game::renderer, &button);
 
     SDL_Color color = {255, 255, 255};
-    SDL_Surface *surface = TTF_RenderText_Solid(HomeScreen::font, this->text, color);
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(Game::renderer, surface);
+    SDL_Surface *surface =
+        TTF_RenderText_Solid(HomeScreen::font, this->text, color);
+    SDL_Texture *texture =
+        SDL_CreateTextureFromSurface(Game::renderer, surface);
 
     SDL_FreeSurface(surface);
     SDL_Rect textRect;
@@ -49,45 +51,44 @@ void Button::render() {
 }
 
 bool Button::isClicked(int mouseX, int mouseY) {
-    return mouseX >= posX && mouseX <= posX + width && mouseY >= posY && mouseY <= posY + height;
+    return mouseX >= posX && mouseX <= posX + width && mouseY >= posY &&
+           mouseY <= posY + height;
 }
 
 void HomeScreen::init() {
-    backgroundHomeScreenTex = TextureManager::LoadTexture("src/gui/assets/backgroundHomeScreen.png");
-    chessLogoTex = TextureManager::LoadTexture("src/assets/chessLogo.png");
+    backgroundHomeScreenTex =
+        TextureManager::LoadTexture("src/gui/assets/backgroundHomeScreen.png");
+    chessLogoTex = TextureManager::LoadTexture("src/gui/assets/chessLogo.png");
     font = TTF_OpenFont(GET("src/gui/assets/fonts/OpenSans.ttf"), 100);
 
-    startBtn.init(
-        Game::getPos(1, false) / 5,
-        Game::getPos(1, true) / 15,
-        100,
-        Game::getPos(1, true) / 5 + Game::getPos(1, true) / 2 - 100,
-        "Start");
-    analysisBtn.init(
-        Game::getPos(1, false) / 5,
-        Game::getPos(1, true) / 15,
-        Game::getPos(1, false) - Game::getPos(1, false) / 5 - 100,
-        Game::getPos(1, true) / 5 + Game::getPos(1, true) / 2 - 100,
-        "Analysis");
+    startBtn.init(Game::getPos(1, false) / 5, Game::getPos(1, true) / 15, 100,
+                  Game::getPos(1, true) / 5 + Game::getPos(1, true) / 2 - 100,
+                  "Start");
+    analysisBtn.init(Game::getPos(1, false) / 5, Game::getPos(1, true) / 15,
+                     Game::getPos(1, false) - Game::getPos(1, false) / 5 - 100,
+                     Game::getPos(1, true) / 5 + Game::getPos(1, true) / 2 -
+                         100,
+                     "Analysis");
     statisticsBtn.init(
-        Game::getPos(1, false) / 5,
-        Game::getPos(1, true) / 15,
-        100,
-        Game::getPos(1, true) / 5 + Game::getPos(1, true) / 2,
-        "Statistics");
+        Game::getPos(1, false) / 5, Game::getPos(1, true) / 15, 100,
+        Game::getPos(1, true) / 5 + Game::getPos(1, true) / 2, "Statistics");
 
-    quitBtn.init(
-        Game::getPos(1, false) / 5,
-        Game::getPos(1, true) / 15,
-        Game::getPos(1, false) - Game::getPos(1, false) / 5 - 100,
-        Game::getPos(1, true) / 5 + Game::getPos(1, true) / 2,
-        "Quit");
+    quitBtn.init(Game::getPos(1, false) / 5, Game::getPos(1, true) / 15,
+                 Game::getPos(1, false) - Game::getPos(1, false) / 5 - 100,
+                 Game::getPos(1, true) / 5 + Game::getPos(1, true) / 2, "Quit");
 }
 
-void HomeScreen::update() {
-}
+void HomeScreen::update() {}
 
 void HomeScreen::handleEvents(SDL_Event *event) {
+    switch (event->type) {
+    case SDL_MOUSEBUTTONUP: {
+        int mouseX, mouseY;
+        SDL_GetMouseState(&mouseX, &mouseY);
+        handleClicks(mouseX, mouseY);
+        break;
+    }
+    }
 }
 
 void HomeScreen::render() {
@@ -95,7 +96,8 @@ void HomeScreen::render() {
     backGroundRect.w = Game::getPos(1, false);
     backGroundRect.h = Game::getPos(1, true);
     backGroundRect.x = backGroundRect.y = 0;
-    TextureManager::Draw(backgroundHomeScreenTex, SDL_Rect{0, 0, 329, 277}, backGroundRect);
+    TextureManager::Draw(backgroundHomeScreenTex, SDL_Rect{0, 0, 329, 277},
+                         backGroundRect);
 
     SDL_Rect chessLogoRect;
     chessLogoRect.w = Game::getPos(1, false) / 3;
@@ -110,23 +112,21 @@ void HomeScreen::render() {
     quitBtn.render();
 
     SDL_Color color = {255, 255, 255};
-    SDL_Surface *surface = TTF_RenderText_Solid(font,
-                                                "Cheese Engine", color);
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(Game::renderer, surface);
+    SDL_Surface *surface = TTF_RenderText_Solid(font, "Cheese Engine", color);
+    SDL_Texture *texture =
+        SDL_CreateTextureFromSurface(Game::renderer, surface);
 
     SDL_FreeSurface(surface);
     SDL_Rect rect;
     rect.w = 500;
     rect.h = 100;
     rect.x = Game::getPos(1, false) / 2 - rect.w / 2;
-    rect.y = 100;
+    rect.y = Game::getPos(0.1, true);
     SDL_RenderCopy(Game::renderer, texture, NULL, &rect);
     SDL_DestroyTexture(texture);
 }
 
-void HomeScreen::cleanUp() {
-    TTF_CloseFont(font);
-}
+void HomeScreen::cleanUp() { TTF_CloseFont(font); }
 
 void HomeScreen::handleClicks(int mouseX, int mouseY) {
     if (startBtn.isClicked(mouseX, mouseY)) {
