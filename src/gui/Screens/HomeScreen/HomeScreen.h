16495c6f4ccdc2ed516be30a47383bf4897e4d90
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-04-12 00:05:48 +0200

    Add screens navigation
    * ScreenHandler class which includes these methods:
       - initialize screen
       - update screen
       - switch screen
       - render screen
       - cleanup screen
       - handle events

--- src/gui/Screens/HomeScreen/HomeScreen.h
+++ src/gui/Screens/HomeScreen/HomeScreen.h
@@ -2,25 +2,25 @@
 
 #include "../../TextureManager/TextureManager.h"
 
-class HomeScreen {
-   private:
-    void handleClicks(int, int);
+class HomeScreen            {
+ private:
+  static void handleClicks(int, int);
 
-   public:
-    static TTF_Font *font;
-    static void init();
-    static void render();
-    static void update();
-    static void handleEvents(SDL_Event *);
-    static void cleanUp();
+ public:
+  static TTF_Font *font;
+  static void init();
+  static void render();
+  static void update();
+  static void handleEvents(SDL_Event *);
+  static void cleanUp();
 };
 
 class Button {
-    int width, height, posX, posY;
+  int width, height, posX, posY;
 
-   public:
-    const char *text;
-    void init(int, int, int, int, const char *);
-    void render();
-    bool isClicked(int, int);
+ public:
+  const char *text;
+  void init(int, int, int, int, const char *);
+  void render();
+  bool isClicked(int, int);
 };
