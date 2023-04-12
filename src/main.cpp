23074e6682ce1ead23223ea9bedffed74703c2da
Youssef El Shabrawii <youssef.elshabrawii@gmail.com>
2023-04-12 14:28:13 +0200

    Configuring Quit Game

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,21 @@
 #include <gui/Game.h>
 
-Game *game = nullptr;
 
-using namespace std;
 
 int main(int argc, char *args[]) {
     long long frameStart;
     int frameTime;
 
-    game = new Game();
-    game->init("Cheese Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HIGHT, 1);
+    Game::init("Cheese Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HIGHT, 1);
 
-    while (game->running()) {
-        game->handleEvents();
-        game->update();
-        game->render();
-        game->handleFPS();
+    while (Game::running()) {
+        Game::update();
+        Game::render();
+        Game::handleFPS();
+        Game::handleEvents();
     }
 
-    game->clean();
+    Game::clean();
 
     return 0;
 }
