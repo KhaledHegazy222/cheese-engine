9b356490a9ce5a8188f6c6c00ff61b5f4a0a0771
Youssef El Shabrawii <youssef.elshabrawii@gmail.com>
2023-04-12 14:28:43 +0200

    Solving Reverse conflict

--- src/gui/Screens/Board.cpp
+++ src/gui/Screens/Board.cpp
@@ -42,17 +42,15 @@ chessBoard::chessBoard() {
         wQueen = TextureManager::LoadTexture("src/gui/assets/wq.png");
         bQueen = TextureManager::LoadTexture("src/gui/assets/bq.png");
     }
-    isReversed = 0;
+    isReversed = 1;
     if (isReversed)
         swap(a1, a2);
     {
         for (int row = 0; row < 8; ++row) {
-            numRow[row] = new Text(24, string(1, '1' + row).c_str(),
-                                   ((row % 2) ? a1 : a2));
+            numRow[row] = new Text(24, string(1, '1' + row).c_str(), ((row % 2) ? a1 : a2));
         }
         for (int col = 0; col < 8; ++col) {
-            numCol[col] = new Text(24, string(1, 'a' + col).c_str(),
-                                   ((col % 2) ? a1 : a2));
+            numCol[col] = new Text(24, string(1, 'a' + col).c_str(), ((col % 2) ? a1 : a2));
         }
     }
 }
@@ -88,10 +86,7 @@ chessBoard::~chessBoard() {
 
 void chessBoard::Render() {
     // Draw Board
-    TextureManager::Draw(this->imageBoard, {0, 0, 1200, 1200},
-                         {Game::getPos(0.22, 0),
-                          (Game::h - Game::getPos(0.48, 0)) / 2,
-                          Game::getPos(0.48, 0), Game::getPos(0.48, 0)});
+    TextureManager::Draw(this->imageBoard, {0, 0, 1200, 1200}, {Game::getPos(0.22, 0), (Game::h - Game::getPos(0.48, 0)) / 2, Game::getPos(0.48, 0), Game::getPos(0.48, 0)});
 
     // Numering the Board
     {
@@ -106,15 +101,9 @@ void chessBoard::Render() {
         }
         for (int col = 0; col < 8; ++col) {
             if (isReversed) {
-                numCol[7 - col]->render(xStart - 3 + (col + 1) * squareLength -
-                                            numCol[col]->destRect.w,
-                                        yStart - 1 + squareLength -
-                                            numCol[col]->destRect.h);
+                numCol[7 - col]->render(xStart - 3 + (col + 1) * squareLength - numCol[col]->destRect.w, yStart - 1 + squareLength - numCol[col]->destRect.h);
             } else {
-                numCol[col]->render(xStart - 3 + (col + 1) * squareLength -
-                                        numCol[col]->destRect.w,
-                                    yStart - 1 + squareLength -
-                                        numCol[col]->destRect.h);
+                numCol[col]->render(xStart - 3 + (col + 1) * squareLength - numCol[col]->destRect.w, yStart - 1 + squareLength - numCol[col]->destRect.h);
             }
         }
     }
@@ -135,59 +124,59 @@ void chessBoard::Render() {
                             squareLength};
                 }
                 switch (state.second) {
-                case Color::White: // White
-                    switch (state.first) {
-                    case Piece::Pawn: // Pawn
-                        TextureManager::Draw(this->wPawn, src, dest);
-                        break;
-                    case Piece::Rock: // Rock
-                        TextureManager::Draw(this->wRock, src, dest);
-                        break;
-                    case Piece::Knight: // Knight
-                        TextureManager::Draw(this->wKnight, src, dest);
-                        break;
-                    case Piece::Bishop: // Bishop
-                        TextureManager::Draw(this->wBishop, src, dest);
-                        break;
-                    case Piece::Queen: // Queen
-                        TextureManager::Draw(this->wQueen, src, dest);
-                        break;
-                    case Piece::King: // King
-                        TextureManager::Draw(this->wKing, src, dest);
-                        break;
+                    case Color::White:   // White
+                        switch (state.first) {
+                            case Piece::Pawn:   // Pawn
+                                TextureManager::Draw(this->wPawn, src, dest);
+                                break;
+                            case Piece::Rock:   // Rock
+                                TextureManager::Draw(this->wRock, src, dest);
+                                break;
+                            case Piece::Knight:   // Knight
+                                TextureManager::Draw(this->wKnight, src, dest);
+                                break;
+                            case Piece::Bishop:   // Bishop
+                                TextureManager::Draw(this->wBishop, src, dest);
+                                break;
+                            case Piece::Queen:   // Queen
+                                TextureManager::Draw(this->wQueen, src, dest);
+                                break;
+                            case Piece::King:   // King
+                                TextureManager::Draw(this->wKing, src, dest);
+                                break;
 
+                            default:
+                                break;
+                        }
+                        break;
+                    case Color::Black:   // Black
+                        switch (state.first) {
+                            case Piece::Pawn:   // Pawn
+                                TextureManager::Draw(this->bPawn, src, dest);
+                                break;
+                            case Piece::Rock:   // Rock
+                                TextureManager::Draw(this->bRock, src, dest);
+                                break;
+                            case Piece::Knight:   // Knight
+                                TextureManager::Draw(this->bKnight, src, dest);
+                                break;
+                            case Piece::Bishop:   // Bishop
+                                TextureManager::Draw(this->bBishop, src, dest);
+                                break;
+                            case Piece::Queen:   // Queen
+                                TextureManager::Draw(this->bQueen, src, dest);
+                                break;
+                            case Piece::King:   // King
+                                TextureManager::Draw(this->bKing, src, dest);
+                                break;
+
+                            default:
+                                break;
+                        }
+                        break;
                     default:
-                        break;
-                    }
-                    break;
-                case Color::Black: // Black
-                    switch (state.first) {
-                    case Piece::Pawn: // Pawn
-                        TextureManager::Draw(this->bPawn, src, dest);
-                        break;
-                    case Piece::Rock: // Rock
-                        TextureManager::Draw(this->bRock, src, dest);
-                        break;
-                    case Piece::Knight: // Knight
-                        TextureManager::Draw(this->bKnight, src, dest);
-                        break;
-                    case Piece::Bishop: // Bishop
-                        TextureManager::Draw(this->bBishop, src, dest);
-                        break;
-                    case Piece::Queen: // Queen
-                        TextureManager::Draw(this->bQueen, src, dest);
-                        break;
-                    case Piece::King: // King
-                        TextureManager::Draw(this->bKing, src, dest);
-                        break;
 
-                    default:
                         break;
-                    }
-                    break;
-                default:
-
-                    break;
                 }
             }
         }
@@ -207,5 +196,7 @@ std::pair<int, int> chessBoard::clickToCell(int posX, int posY) {
     int minY = yStart - 7 * squareLength;
     int row = 7 - (posY - minY) / squareLength;
     int column = (posX - minX) / squareLength;
+    row = ((isReversed) ? 7 - row : row);
+    column = ((isReversed) ? 7 - column : column);
     return std::make_pair(row, column);
 }
